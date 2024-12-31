b4aba33db6016c219dda1ddb557c422b7bbf2a98
Seeple <sjtu_fy@sjtu.edu.cn>
2024-12-31 23:24:39 +0800

    groundingDINO inference modified version

--- GD_inference.py
+++ GD_inference.py
@@ -1,106 +1,60 @@
 '''
-Inference UI dataset on pretrained model of GroundingDINO
-This file gives a modified version catering to our new pipeline
+Inference on GroundingDINO
 '''
-import argparse
-from datetime import datetime
-from tqdm import tqdm
-from torch.utils.data import DataLoader
 import os
+import json
+import torch
+import numpy as np
 
-from utils.GD_utils import plot_boxes_to_image_no_label, load_image_from_dataset, load_model, get_grounding_output
-from dataset.datasetGD import UIDataset
+from utils.GD_utils import load_image, load_model, get_grounding_output
 
-def main():
-    
-    # add args from parser
-    parser = argparse.ArgumentParser("Grounding DINO example", add_help=True)
-    parser.add_argument("--config_file", "-c", type=str, default="groundingdino/config/GroundingDINO_SwinT_OGC.py", help="path to config file")
-    parser.add_argument(
-        "--checkpoint_path", "-p", type=str, default="weights/groundingdino_swint_ogc.pth", help="path to checkpoint file"
-    )
-    parser.add_argument("--box_threshold", type=float, default=0.1, help="box threshold")
-    parser.add_argument("--text_threshold", type=float, default=0.1, help="text threshold")
-    parser.add_argument("--cpu-only", default=True, help="running on cpu only!, default=False")
-    args = parser.parse_args()
-    
-    # intialize configs from args
-    config_file = args.config_file  # change the path of the model config file
-    checkpoint_path = args.checkpoint_path  # change the path of the model
-    box_threshold = args.box_threshold
-    text_threshold = args.text_threshold
+def GD_inference(dir, text_prompt = "UIBox . icon ."):
+    '''
+    Input: 
+    dir: path to the original image
+    text: text prompt input of Grounding DINO 
+    '''
+    config_file = "third_party/UIGD/config/GroundingDINO_SwinT_OGC.py"
+    checkpoint_path = "third_party/UIGD/weights/groundingdino_swint_ogc.pth"
+    box_threshold = 0.1
+    text_threshold = 0.15
     
     # initialize the model
-    model = load_model(config_file, checkpoint_path, cpu_only=args.cpu_only)
+    model = load_model(config_file, checkpoint_path, cpu_only=True)
     
-    # set the root directory to save all the predicted images(by system time)\
-    now = datetime.now()
-    timestamp = now.strftime("%Y-%m-%d-%H-%M-%S")
-    base_log_path = f"./logs/{timestamp}"
-
-    # initialize dataloader
-    category_path = "./data/categories.txt"
-    input_shape = (1920, 1280)
-    batch_size = 1
-    # test_dataset = UIDataset(data_path="./data", category_path=category_path, input_shape=input_shape) 
-    test_dataset = UIDataset(data_path="./data", category_path=category_path, input_shape=input_shape) 
-    test_loader = DataLoader(test_dataset, shuffle=True, batch_size=batch_size, num_workers=0, pin_memory=True)
-    demo_dataset = test_dataset[:5]
-    demo_loader = DataLoader(demo_dataset, shuffle=True, batch_size=batch_size, num_workers=0, pin_memory=True)
+    # load image 
+    image_path = dir + ".png"
+    image_pil, image = load_image(image_path)
     
-    # load data iteratively from dataloader, and return result dict
-    tbar = tqdm(test_loader)
-    # tbar = tqdm(demo_loader)
-    res_list = []
-    step = 1
-    for images in tbar:
-        '''
-        (B * data_size, B=1 by default)
-        '''
-        for i in range(images.size()[0]):
-            # the saving directory of each image should be under base_log_path
-            image = images[i]
-            text_prompt = 'ClickableUIBox . ScrollableUIBox . SelectableUIBox . Icon' # WE DONT CARE CATEGORIES IN NEW PIPELINE 
-            
-            # define output directory based on the basename of the image
-            img_name = f'image_{step}'
-            output_dir = os.path.join(base_log_path, img_name)  
-            os.makedirs(output_dir, exist_ok=True)
-            step += 1
-            
-            # load image
-            image_pil, image = load_image_from_dataset(image)
-            # save raw image
-            image_pil.save(os.path.join(output_dir, "raw_image.jpg"))
-            
-            # run model
-            boxes_filt, pred_phrases = get_grounding_output(model, image, text_prompt, box_threshold, text_threshold, cpu_only=args.cpu_only, nms=True)
-            
-            # visualize and save the pred
-            size = image_pil.size 
-            pred_dict = {
-                "boxes": boxes_filt,
-                "size": [size[1], size[0]],  # H,W
-            }
-        
-            image_with_box = plot_boxes_to_image_no_label(image_pil, pred_dict)[0]
-            image_with_box.save(os.path.join(output_dir, "pred.jpg"))
-            res_list.append(pred_dict)
-            
-    '''
-    res_list is a list of the prediction results of each image
+    # run model
+    boxes_filt, _ = get_grounding_output(model, image, text_prompt, box_threshold, text_threshold, cpu_only = True, nms=True)
+    
+    # convert the result to xyxy and save in .json
+    size = image_pil.size 
     pred_dict = {
         "boxes": boxes_filt,
         "size": [size[1], size[0]],  # H,W
-        "labels": pred_phrases,
     }
-    Here:
-    boxes contain (center_x, center_y, width, height). 
-    These values are normalized in [0, 1].
-    denormalize it through "size"
-    '''
-        
-    print('all the samples have been inferenced!')
+    
+    bboxes = []
+    H, W = size[1], size[0]
+    for box in boxes_filt:
+        box = box * torch.Tensor([W, H, W, H])
+        # from xywh to xyxy
+        box[:2] -= box[2:] / 2
+        box[2:] += box[:2]
+        x0, y0, x1, y1 = box
+        x0, y0, x1, y1 = int(x0), int(y0), int(x1), int(y1)
+        bboxes.append([x0, y0, x1, y1])
+    
+    with open(dir + ".json", 'w') as f:
+        json.dump(bboxes, f, indent=4)
+
+def main():
+    
+    img_dir = "./data/coredraw/frame_1"
+    GD_inference(img_dir)
+    
     
 if __name__ == '__main__':
     main()
